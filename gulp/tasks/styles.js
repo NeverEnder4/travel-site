fe947fcd94ac14779b494aa49395d6b6ae78ca6c
Tony <apettigrew.wsdev@gmail.com>
2018-05-05 16:02:22 -0700

    Made hero image responsive, added postcss mixins to gulp workflow, added media queries via mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,13 +1,18 @@
+//IMPORT REQUIRED PACKAGES
 var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
-    cssImport = require('postcss-import');
+    cssImport = require('postcss-import'),
+    mixins = require('postcss-mixins');
 
+//TASK THAT HOLDS OUR POSTCSS FILTERS
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+
+        //WHEN AN ERROR OCCURS, PRINT INFORMATION ABOUT THE ERROR, KEEP RUNNING WATCH TASK
         .on('error', function(errorInfo){
             console.log(errorInfo.toString());
             this.emit('end');
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,25 +1,35 @@
+//IMPORT REQUIRED PACKAGES
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     browserSync = require('browser-sync').create();
 
+
+//TASK THAT WATCHES OUR INDEX.HTML, CSS FOLDERS/FILES AND POINTS THE SERVER TO INDEX.HTML FOLDER LOCATION
 gulp.task('watch', function () {
 
     browserSync.init({
+        
+        //TURNED OF NOTIFICATIONS
         notify: false,
+
+        //TELLS THE SERVER WHICH FOLDER TO POINT TO
         server: {
             baseDir: 'app'
         }
     });
 
+    //WHEN THERE IS A CHANGE TO INDEX.HTML RELOAD THE BROWSER
     watch('./app/index.html', function () {
         browserSync.reload();
     });
 
+    //WHEN THERE IS A CHANGE TO ANY CSS FOLDER OR FILE INJECT THE CHANGES WITHOUT RELOADING
     watch('./app/assets/styles/**/*.css', function () {
         gulp.start('cssInject');
     });
 });
 
+//WHEN STYLES TASK HAS FINISHED, RUN CSS INJECT
 gulp.task('cssInject', ['styles'], function () {
     return gulp.src('./app/temp/styles/styles.css')
         .pipe(browserSync.stream());
