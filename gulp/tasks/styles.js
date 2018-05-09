68467a6ff442e22030d18ae33e06f014be22783f
Tony <apettigrew.wsdev@gmail.com>
2018-05-09 10:00:18 -0700

    Finished header styling, will need to add JS to make the menu functional

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,12 +5,13 @@ var gulp = require('gulp'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
-    mixins = require('postcss-mixins');
+    mixins = require('postcss-mixins'),
+    hexrgba = require('postcss-hexrgba');
 
 //TASK THAT HOLDS OUR POSTCSS FILTERS
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested,hexrgba, autoprefixer]))
 
         //WHEN AN ERROR OCCURS, PRINT INFORMATION ABOUT THE ERROR, KEEP RUNNING WATCH TASK
         .on('error', function(errorInfo){
